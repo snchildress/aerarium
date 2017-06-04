d83d188aef2a005f644b6a29a4b06b5ec0816d47
Steven Childress <steven.n.childress@gmail.com>
2017-06-04 18:46:51 -0500

    Create converstion rate update script

---
+++ source/source_settings.py
@@ -0,0 +1,8 @@
+# API endpoints
+
+# Currency API
+CURRENCY_BASE_URL = 'https://api.fixer.io/'
+
+CURRENCY_ENDPOINTS = {
+    'convert_to_usd': CURRENCY_BASE_URL + '{}?base={}&symbols=USD', # Date & Currency
+}
--- source/views.py
+++ source/views.py
@@ -1,4 +1,37 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import date, timedelta
+import requests
+
+from source.models import Currency
+from source import source_settings
 
 
-# Create your views here.
+### External functions
+
+def home(request):
+    update_currencies()
+    return HttpResponse('Successfully updated yesterday\'s currencies!')
+
+### Internal functions
+
+def update_currencies():
+    # Loops through all conversions in the database
+    for currency in Currency.CURRENCY_TYPES:
+        currency = currency[0] # Take first value in currency_type tuple
+        convert_to_usd(currency)
+
+def convert_to_usd(currency):
+    # Writes conversion rate to USD for given currency to the database
+    yesterday = date.today() - timedelta(1)
+    currency_endpoint = source_settings.CURRENCY_ENDPOINTS['convert_to_usd'].format(yesterday, currency)
+    response = requests.get(currency_endpoint).json()
+    currency = response['base']
+    conversion_rate = response['rates']['USD']
+    conversion_rate_date = response['date']
+    if conversion_rate_date == yesterday: # Prevents duplicate Friday data
+        currency_record = Currency(
+            currency=currency,
+            conversion_rate=conversion_rate,
+            conversion_rate_date=conversion_rate_date
+        )
+        currency_record.save()
